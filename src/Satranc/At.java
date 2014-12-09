59bfc1fc4cdf2960fc98d745e1c89cec3982bab2
emrekose26 <emre.kose2@ogr.sakarya.edu.tr>
2014-12-09 19:28:49 +0200

    update 5
    asdada

--- src/Satranc/At.java
+++ src/Satranc/At.java
@@ -5,6 +5,50 @@ public class At extends Tas {
 	@Override
 	public void hamleYap(Koordinat koordinat, Tahta tahta) {
 		
+		int yFark =Math.abs(koordinat.getbasX() - koordinat.getsonX());
+		int xFark =Math.abs(koordinat.getbasY() - koordinat.getsonY());
+		
+		int Degistir = 0;
+		//1.oyuncunun at için oynama durumlarý
+		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()] == 'a')
+		{
+			if(((xFark == 1 && yFark == 2) || (xFark == 2 && yFark == 1)))
+			{
+				//oynayacaðý koordinatýn boþ olma ve rakip taþý bulundurma durumu(yeme)
+				if(tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == ' ' || tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'P' || tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'K'|| tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'A' || 
+						tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'F'|| tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'V')
+				{
+					Degistir = 1;
+				}
+			}
+			
+		}
+		
+		//2.oyuncunun at için oynama durumlarý
+		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()] == 'A')
+		{
+			if(((xFark == 1 && yFark == 2) || (xFark == 2 && yFark == 1)))
+			{
+				if(tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == ' ' || tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'p' || tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'k'|| tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'a' || 
+						tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'f'|| tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] == 'v')
+				{
+					Degistir = 2;
+				}
+			}
+		}
+		
+		//1. ve 2.oyuncunun oynayabilme durumlarý
+		if(Degistir == 1)
+		{
+			tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] = 'a';
+			tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+		}
+		
+		else if(Degistir == 2)
+		{
+			tahta.tahta[koordinat.getsonY()][koordinat.getsonX()] = 'A';
+			tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=' ';
+		}
 		
 	}
 
