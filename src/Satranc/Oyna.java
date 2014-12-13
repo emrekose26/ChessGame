95d049589041de73e5699d1d91cdf635a3298d16
emrekose26 <e.kose26@gmail.com>
2014-12-13 16:52:39 +0200

    Oyna sınıfına hata durumları eklendi

--- src/Satranc/Oyna.java
+++ src/Satranc/Oyna.java
@@ -5,65 +5,112 @@ import java.util.Scanner;
 public class Oyna {
 
 	public static void main(String[] args) {
+		boolean hata1=false;
+		boolean hata1_1=false;
+		boolean hata1_2=false;
+		boolean hata2=false;
+		boolean hata2_1=false;
+		boolean hata2_2=false;
 		Tahta tahta = new Tahta();
 		Koordinat koordinat = new Koordinat();
 		Oyuncu beyaz = new Oyuncu();
 		Oyuncu siyah = new Oyuncu();
 		
-		boolean hata1 = false , hata2 = false;
 		
-		while(true){
-			
-			if(hata2!=true)  
+		while(true)
+		{
+
+			if(hata2!=true && hata2_1!=true && hata2_2!=true )  
 			{
 
-		    tahta.yazdir();
-		    System.out.println();
+		    tahta.yazdir(); System.out.println();
 			System.out.println("Beyaz taþlý oyuncumuz sýra sizin,oynatmak istediðiniz taþýn X koordinatý?");
 	    	koordinat.setbasX(new Scanner(System.in).nextByte());
 	    	System.out.println("Y koordinatý lütfen ?"); koordinat.setbasY(new Scanner(System.in).nextByte());
 	    	System.out.println("Götürmek istediðiniz x koordinatý?"); koordinat.setsonX(new Scanner(System.in).nextByte());
 	    	System.out.println("Götürmek istediðiniz y koordinatý ?"); koordinat.setsonY(new Scanner(System.in).nextByte());
 			
-	    	if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='\0') 
-	    		{ hata1=true;  System.out.println("Hatalý koordinat seçimi"); }
+	    	if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='\0')  //boþ koordiinat veya rakip taþý seçme durumunda 
+	    		{ hata1=true;  System.out.println("Hatalý koordinat seçimi"); System.out.println(); }
+	    	else 
+	    		hata1=false;
 	    	
-	    	if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]!='\0')
+	    	if(Character.isUpperCase(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()])==true){
+	    	    hata1_1=true;  System.out.println("Hatalý seçim! Rakip taþý oynayamazsýnýz");
+	    	}
+	    	else 
+	    		hata1_1=false;
+	    	
+	    	
+	    	if(hata1_1!=true && tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]!='\0')
 	    	  {
-	    		hata1=false; beyaz.koordinatiAl(koordinat);     beyaz.tahtayiAl(tahta);       beyaz.tahtayaGonder();  
-	    	  }
+	    		 
+	    		beyaz.koordinatiAl(koordinat);    
+	    		beyaz.tahtayiAl(tahta);      
 	    		
-			} 
+	    		   if(beyaz.tahtayaGonder()) 
+	    		      {
+	    			    System.out.println("hatalý koordinat giriyorsunuz "); 
+	    			    hata1_2=true;  
+	    		      }
+	    		   else 
+	    		   {
+	    			   hata1=false;  hata1_1=false;  hata1_2=false;
+	    		   }
+	    	  }
+	    	
+	      }
+	    		
+			
 			
 
 			 
 			//Siyah için beyaz hata yapmamalý
 			
-	    	if(hata1!=true)
+	    	if(hata1!=true && hata1_1!=true && hata1_2!=true )
 	    	{
 
-				tahta.yazdir();
-				System.out.println();
-				
+				tahta.yazdir(); System.out.println();
 	    		System.out.println("Siyah taþlý oyuncumuz sýra sizin,oynatmak istediðiniz taþýn X koordinatý?");
 		    	koordinat.setbasX(new Scanner(System.in).nextByte());
 		    	System.out.println("Y koordinatý lütfen ?"); koordinat.setbasY(new Scanner(System.in).nextByte());
 		    	System.out.println("Götürmek istediðiniz x koordinatý?"); koordinat.setsonX(new Scanner(System.in).nextByte());
 		    	System.out.println("Götürmek istediðiniz y koordinatý ?"); koordinat.setsonY(new Scanner(System.in).nextByte());
 	    	  	
-	    		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='\0')
-	    		     { hata2=true;   System.out.println("Hatalý koordinat"); }
+	    		     if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]=='\0' )
+	    		     { hata2=true;   System.out.println("Hatalý koordinat"); System.out.println();}
+	    		     else
+	    		    	 hata2=false;
 	    		
-	    		if(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]!='\0')
-		    	  {
-		    		hata2=false;   beyaz.koordinatiAl(koordinat);     beyaz.tahtayiAl(tahta);       beyaz.tahtayaGonder();   tahta.yazdir();
-		    	  }
-	    		System.out.println();
+	    		     if(Character.isLowerCase(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()])==true){
+	    		    	 hata2_1=true;  System.out.println("Hatalý seçim! Rakip taþý oynayamazsýnýz ");
+	    		     }
+	    		    	    
+	    		    	else if(Character.isUpperCase(tahta.tahta[koordinat.getbasY()][koordinat.getbasX()])==true)
+	    		    		hata2_1=false;
+	    		     
+	    		      if( hata2_1==false && tahta.tahta[koordinat.getbasY()][koordinat.getbasX()]!='\0')
+		    	          {
+		    		       
+		    		        siyah.koordinatiAl(koordinat);  
+		    		        siyah.tahtayiAl(tahta);     
+		    		
+		    		             if(siyah.tahtayaGonder())
+		    		                {
+		    			                System.out.println("hatalý koordinat giriyorsunuz "); 
+		    		                    hata2=true; //yanlýþ koordinat girmiþse 
+		    		                }
+		    		             else 
+		    		             {
+		    		            	 hata2=false; hata2_1=false; hata2_2=false;
+		    		             }
+		    		
+		    	          }
+	 
 	    		
-	    	}
+	    	 }
 	    	
-	    	
-	    	
+	    	 
 		}
 
 	}
